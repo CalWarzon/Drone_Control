66b91ecf24939be760308b5456509a21fe07e7b0
CalWarzon <warzoncal@gmail.com>
2026-07-18 16:42:08 -0400

    PIDing

--- main.py
+++ main.py
@@ -292,9 +292,9 @@ safe = Safety()
 
 
 fc = FC(pitchPID = PID(
-            kp = .002,
-            ki = .0004,
-            kd = .0007,
+            kp = .0005,
+            ki = .000,
+            kd = .000,
             integrator_limit=.3,
             output_limit=.2,
             integral_fade=0.97,
@@ -302,9 +302,9 @@ fc = FC(pitchPID = PID(
             use_gyro_derivative=True
         ), 
         rollPID = PID(
-            kp = .002,
-            ki = .0004,
-            kd = .0009,
+            kp = .0005,
+            ki = .000,
+            kd = .0000,
             integrator_limit=.3,
             output_limit=.2,
             integral_fade=0.97,
