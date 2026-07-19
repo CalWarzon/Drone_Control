dd2f4f3fe88842a80825cef87db2a16aa60430ac
CalWarzon <warzoncal@gmail.com>
2026-07-19 12:46:49 -0400

    Debugging

--- main.py
+++ main.py
@@ -167,6 +167,7 @@ def FlightControlLoop(fc, xbox, safety,
         elif not slowStartDone:
             if t.perf_counter() - slowStartTime < 4:
                 fc.SetPWM([.1*(t.perf_counter() - slowStartTime)/4 * 1000 + 1000] * 4)
+                print(.1*(t.perf_counter() - slowStartTime)/4 * 1000 + 1000)
                 continue
             else:
                 slowStartDone = True
